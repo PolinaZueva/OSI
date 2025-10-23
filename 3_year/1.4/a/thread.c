bfa7b521d45ea6fc13b5f70925d5aec9f4ec9e42
PolinaZueva <polina@DESKTOP-75L662O.localdomain>
2025-10-23 09:09:11 +0700

    add 1.4

---
+++ 3_year/1.4/a/thread.c
@@ -0,0 +1,53 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define RETURN_CODE 0
+#define ERROR -1
+
+void *mythread(void *arg) {
+	while (true) {
+        printf("mythread [%d %d %d]: sends a message...\n", getpid(), getppid(), gettid());
+        sleep(1);
+    }
+    return NULL;
+}
+
+int main() {
+	pthread_t tid;
+	int err;
+	void *thread_ret_result;
+
+	printf("main [%d %d %d]: Hello from main\n", getpid(), getppid(), gettid());
+
+	err = pthread_create(&tid, NULL, mythread, NULL);
+	if (err != RETURN_CODE) {
+	    printf("main: pthread_create() failed: %s\n", strerror(err));
+		return ERROR;
+	}
+
+    sleep(3);
+    
+    err = pthread_cancel(tid);
+    if (err != RETURN_CODE) {
+        printf("main: pthread_cancel() failed: %s\n", strerror(err));
+		return ERROR;
+    }	
+    
+    err = pthread_join(tid, &thread_ret_result);
+    if (err != RETURN_CODE) {
+        printf("main: pthread_join() failed: %s\n", strerror(err));
+		return ERROR;
+    }	
+
+    if (thread_ret_result == PTHREAD_CANCELED) {
+        printf("main [%d %d %d]: thread was canceled\n", getpid(), getppid(), gettid());
+    }    
+	return RETURN_CODE;
+}
---
+++ 3_year/1.4/c/thread.c
@@ -0,0 +1,73 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define RETURN_CODE 0
+#define ERROR -1
+
+void free_message_cleanup(void *arg) {
+    char *message_ptr = (char *)arg;
+    if (message_ptr != NULL) {
+        printf("cleanup: free memory\n");
+        free(message_ptr);
+    }
+}
+
+void *mythread(void *arg) {
+    char* msg = "hello world";	
+	char* message = malloc(strlen(msg) + 1);
+	if (message == NULL) {
+		printf("mythread: malloc failed\n");
+		return NULL;
+	}
+	strcpy(message, msg);
+
+    pthread_cleanup_push(free_message_cleanup, message);
+    
+	while (true) {
+        printf("mythread [%d %d %d]: %s\n", getpid(), getppid(), gettid(), message);
+        pthread_testcancel();
+        sleep(1);
+    }
+    pthread_cleanup_pop(0);
+    return NULL;
+}
+
+int main() {
+	pthread_t tid;
+	int err;
+	void *thread_ret_result;
+
+	printf("main [%d %d %d]: Hello from main\n", getpid(), getppid(), gettid());
+
+	err = pthread_create(&tid, NULL, mythread, NULL);
+	if (err != RETURN_CODE) {
+	    printf("main: pthread_create() failed: %s\n", strerror(err));
+		return ERROR;
+	}
+
+    sleep(3);
+    
+    err = pthread_cancel(tid);
+    if (err != RETURN_CODE) {
+        printf("main: pthread_cancel() failed: %s\n", strerror(err));
+		return ERROR;
+    }	
+    
+    err = pthread_join(tid, &thread_ret_result);
+    if (err != RETURN_CODE) {
+        printf("main: pthread_join() failed: %s\n", strerror(err));
+		return ERROR;
+    }	
+
+    if (thread_ret_result == PTHREAD_CANCELED) {
+        printf("main [%d %d %d]: thread was canceled\n", getpid(), getppid(), gettid());
+    }    
+	return RETURN_CODE;
+}
